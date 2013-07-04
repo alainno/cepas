f5b418b70c465c4ae9567aeda7a625e31cd59544
Diseño <Diseo@Diseo-PC>
2013-07-04 16:20:09 -0500

    commit

--- single-producto.php
+++ single-producto.php
@@ -32,36 +32,36 @@ get_header();
                                 </nav>
                             </div>-->
                 <article>
-                        <h2><?php the_title(); ?></h2>
-                        <?php
-                        $thumb_id = get_post_thumbnail_id();
-                        $thumb_url = wp_get_attachment_image_src($thumb_id, 'thumbnail', true);
-						$content = get_the_content();
-						$_images = getImages($content);
-						$images = $_images[0];
-						
-						$_precio = get_post_meta(get_the_ID());
-						$precio = $_precio["Precio"][0];
-                        ?>
-						<div class="clearer">
-                        <!--<img id="img-cat-main" src="<?php echo $thumb_url[0]; ?>" class="left" width="495" height="420" />-->
+                    <h2><?php the_title(); ?> <img id="etiq" src="<?php echo get_template_directory_uri(); ?>/img/img-etiqueta.png" class="" width="142" height="25" /><p class="precio">S/. 12.40</p></h2>
+                    <div class="line-etiq"></div>
+                    <?php
+                    $thumb_id = get_post_thumbnail_id();
+                    $thumb_url = wp_get_attachment_image_src($thumb_id, 'thumbnail', true);
+                    $content = get_the_content();
+                    $_images = getImages($content);
+                    $images = $_images[0];
+
+                    $_precio = get_post_meta(get_the_ID());
+                    $precio = $_precio["Precio"][0];
+                    ?>
+                    <div class="clearer">
+        <!--<img id="img-cat-main" src="<?php echo $thumb_url[0]; ?>" class="left" width="495" height="420" />-->
                         <img id="img-cat-main" src="<?php echo get_theme_template; ?>" class="left" width="495" height="420" />
                         <div class="imgs-children right">
                             <ul>
-								<?php
-								for($i=0; $i<sizeof($images); $i++)
-								{
-								?>
-                                <li><?php echo $images[$i]?></li>
-								<?php
-								}
-								?>
+                                <?php
+                                for ($i = 0; $i < sizeof($images); $i++) {
+                                    ?>
+                                    <li><?php echo $images[$i] ?></li>
+                                    <?php
+                                }
+                                ?>
 
                             </ul>
                         </div>
-						</div>
-                        <p><?php echo removeImages($content); ?></p>
-                        <p class="precio"><?php echo $precio; ?></p>
+                    </div>
+                    <p><?php echo removeImages($content); ?></p>
+                    <p class="precio"><?php echo $precio; ?></p>
                 </article>
                 <div class="paginacion clearer mt15">
                     <div class="left"><?php next_post_link('%link', '&laquo; Anterior', true); ?></div>
